632e91bb36bfa4a11b57d9bce1664959afbe1dbe
nguyenlong-github <nguyenhailong.works@gmail.com>
2024-12-25 22:49:50 +0900

    Update User Model and Views.py

--- fit_your_style/myapp/admin.py
+++ fit_your_style/myapp/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Appointment
+
+from .models import Appointment, User
 
 # Register your models here.
 
 class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ("name", "surname", "date", "time",)
+    list_display = ("id", "date", "time",)
 admin.site.register(Appointment,AppointmentAdmin)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "last_name", "first_name")
+admin.site.register(User,UserAdmin)
--- fit_your_style/myapp/models.py
+++ fit_your_style/myapp/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+import uuid
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    last_name = models.CharField(max_length=30, verbose_name='名前')
+    first_name = models.CharField(max_length=30, verbose_name='姓')
+    email = models.EmailField(max_length=254, verbose_name='メールアドレス')
+    address = models.CharField(max_length=100, null=True, blank=True, verbose_name='住所')
+    phone = models.CharField(max_length=15, null=True, blank=True, verbose_name='電話番号')
+    avatar = models.ImageField(upload_to='myapp/avatar', null=True, blank=True)
+    password = models.CharField(max_length=30, verbose_name='パスワード')
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
 
 class Appointment(models.Model):
-    surname = models.CharField(max_length=30, verbose_name='姓')
-    name = models.CharField(max_length=30,verbose_name='名')
-    email = models.EmailField(verbose_name='メールアドレス')
-    phone = models.CharField(max_length=15, null=True, blank=True, verbose_name='電話番号')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     date = models.DateField(verbose_name='日付')
     time = models.TimeField(verbose_name='時刻')
     status = models.CharField(max_length=10,verbose_name='ステータス',default='pending')
@@ -13,4 +23,4 @@ class Appointment(models.Model):
     create_at = models.DateTimeField(auto_now_add=True, verbose_name="作成時間")
     update_at = models.DateTimeField(auto_now_add=True, verbose_name="変更時間")
     def __str__(self):
-        return f"{self.name} {self.surname}"
+        return f"{self.id} {self.date} {self.time}"
